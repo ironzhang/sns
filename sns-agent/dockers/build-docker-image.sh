87b0381cc834b956cb5ea2a09ab2f6c397742ecf
zhanghui <iron.zhang@foxmail.com>
2024-07-22 20:14:36 +0800

    update sns-agent build docker script

--- sns-agent/dockers/build-docker-image.sh
+++ sns-agent/dockers/build-docker-image.sh
@@ -6,5 +6,6 @@ cp $MINIKUBE/ca.crt ./conf/kube/
 cp $MINIKUBE/profiles/minikube/client.crt ./conf/kube/
 cp $MINIKUBE/profiles/minikube/client.key ./conf/kube/
 
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build ../../tools/sns-lookup/
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build ../
 docker build -t sns/agent:v0.0.1 .
--- sns-agent/dockers/entrypoint.sh
+++ sns-agent/dockers/entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+mkdir -p /root/.supername/bin
+cp ./sns-lookup /root/.supername/bin/
 ./sns-agent -config ./conf/cfg.docker.toml >run.log 2>&1
